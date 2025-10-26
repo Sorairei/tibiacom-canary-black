3f2f09f63d2f206008ab87b1d3d89d96aa10e45a
Sorairei <79299476+Sorairei@users.noreply.github.com>
2025-10-25 23:13:03 -0600

    Refactor headline image generation and output format
    Simplifies headline image generation by removing commented legacy code, increasing image width to 600px, and switching output format from GIF to PNG. Also improves input handling and variable usage for clarity, fix black background in headline images.

--- headline.php
+++ headline.php
@@ -1,42 +1,23 @@
 <?php
-//require '../../common.php';
-
-//$text = $_GET['text'];
-/*
-$page_file = BASE . 'pages/' . PAGE . '.php';
-if(!@file_exists($page_file))
-{
-	$page_file = BASE . 'pages/custom/' . PAGE . '.php';
-	if(!@file_exists($page_file))
-		die('Page does not exists.');
-}
-*/
-//$file = 'images/header/headline-' . PAGE . '.gif';
-//if(!file_exists($file))
-//{
-if (strlen($_GET['t']) > 100) // max limit
-    $_GET['t'] = '';
+$text = $_GET['t'];
+if(strlen($text) > 100) // max limit
+	$text = '';
 
 // set font path
 putenv('GDFONTPATH=' . __DIR__);
 
 // create image
-$image = imagecreatetruecolor(250, 28);
+$image = imagecreatetruecolor(600, 28);
 
 // make the background transparent
 imagecolortransparent($image, imagecolorallocate($image, 0, 0, 0));
 
 // set text
 $font = getenv('GDFONTPATH') . DIRECTORY_SEPARATOR . 'martel.ttf';
-imagettftext($image, 18, 0, 4, 20, imagecolorallocate($image, 240, 209, 164), $font, $_GET['t']);
+imagettftext($image, 18, 0, 4, 20, imagecolorallocate($image, 240, 209, 164), $font, $text);
 
 // header mime type
-header('Content-type: image/gif');
+header('Content-type: image/png');
 
-// save image
-imagegif($image/*, $file*/);
-//}
-
-// output image
-//header('Content-type: image/gif');
-//readfile($file);
+// output image to browser
+imagepng($image);
